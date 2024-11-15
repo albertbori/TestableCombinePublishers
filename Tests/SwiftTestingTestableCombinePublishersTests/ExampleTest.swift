844a0d7fa54e9fed410f77fd2eee0cc7998631b0
Ethan van Heerden <evanheerden@wayfair.com>
2024-11-15 15:05:34 -0500

    Uncomment code

---
+++ Tests/SwiftTestingTestableCombinePublishersTests/ExampleTest.swift
@@ -0,0 +1,30 @@
+//
+//  ExampleTest.swift
+//  TestableCombinePublishers
+//
+//  Created by Ethan van Heerden on 11/15/24.
+//
+
+import Combine
+import Testing
+
+struct ExampleTest {
+
+    @Test func fail() async {
+        await withKnownIssue() {
+            let publisher = CurrentValueSubject<String, Error>("foo")
+            await publisher
+                .expect("bar")
+                .expectSuccess()
+                .waitForExpectations(timeout: 1)
+        }
+    }
+    
+    @Test func pass() async {
+        let publisher = ["baz"].publisher
+        await publisher
+            .expect("baz")
+            .expectSuccess()
+            .waitForExpectations(timeout: 1)
+    }
+}
