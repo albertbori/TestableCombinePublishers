9cef2b28ae60adb7da77b95353b3d7760cb32e8f
Ethan van Heerden <evanheerden@wayfair.com>
2024-11-27 15:09:30 -0500

    Refix test

--- Tests/SwiftTestingTestableCombinePublishersTests/ExampleTest.swift
+++ Tests/SwiftTestingTestableCombinePublishersTests/ExampleTest.swift
@@ -4,12 +4,14 @@ import Testing
 struct ExampleTest {
     
     @Test func fail() async {
-        let publisher = CurrentValueSubject<String, Error>("foo")
-        await publisher
-            .testable()
-            .expect("bar")
-            .expectSuccess()
-            .waitForExpectations(timeout: 1)
+        await withKnownIssue {
+            let publisher = CurrentValueSubject<String, Error>("foo")
+            await publisher
+                .testable()
+                .expect("bar")
+                .expectSuccess()
+                .waitForExpectations(timeout: 1)
+        }
     }
     
     @Test func pass() async {
