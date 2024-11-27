2ec5f047cb83902b3ca96a90e75aa27366b6f21b
Ethan van Heerden <evanheerden@wayfair.com>
2024-11-27 15:06:00 -0500

    Fix image size

--- Tests/SwiftTestingTestableCombinePublishersTests/ExampleTest.swift
+++ Tests/SwiftTestingTestableCombinePublishersTests/ExampleTest.swift
@@ -1,24 +1,15 @@
-//
-//  ExampleTest.swift
-//  TestableCombinePublishers
-//
-//  Created by Ethan van Heerden on 11/15/24.
-//
-
 import Combine
 import Testing
 
 struct ExampleTest {
-
+    
     @Test func fail() async {
-        await withKnownIssue() {
-            let publisher = CurrentValueSubject<String, Error>("foo")
-            await publisher
-                .testable()
-                .expect("bar")
-                .expectSuccess()
-                .waitForExpectations(timeout: 1)
-        }
+        let publisher = CurrentValueSubject<String, Error>("foo")
+        await publisher
+            .testable()
+            .expect("bar")
+            .expectSuccess()
+            .waitForExpectations(timeout: 1)
     }
     
     @Test func pass() async {
@@ -30,3 +21,5 @@ struct ExampleTest {
             .waitForExpectations(timeout: 1)
     }
 }
+
+
